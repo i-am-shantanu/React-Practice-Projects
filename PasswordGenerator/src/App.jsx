63adb4304d966d536c16b7ae7cc8936b66af290f
Shantanu <shantanumishrajuly77@gmail.com>
2024-01-01 13:53:36 +0530

    setup

---
+++ PasswordGenerator/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='titlebar'>
+      Password Generator
+    </div>
+    <div className='container'>
+        
+    </div>
+    </>
+  )
+}
+
+export default App
