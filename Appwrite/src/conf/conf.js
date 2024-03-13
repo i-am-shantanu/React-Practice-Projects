a87a3727c6cc5283c04ec469bff917ed283d7372
Shantanu <shantanumishrajuly77@gmail.com>
2024-03-13 16:05:16 +0530

    Signed-off-by: Shantanu <shantanumishrajuly77@gmail.com>

--- Appwrite/src/conf/conf.js
+++ Appwrite/src/conf/conf.js
@@ -1,7 +1,7 @@
 
 const conf ={
     appWriteUrl:String(process.env.REACT_APP_APPWRITE_URL),
-    appWriteProjectId:Strinng(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appWriteProjectId:String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
     appWriteDatabaseId:String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
     appWriteCollectionId:String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
     appWriteBucketId:String(process.env.REACT_APP_APPWRITE_BUCKET_ID)
